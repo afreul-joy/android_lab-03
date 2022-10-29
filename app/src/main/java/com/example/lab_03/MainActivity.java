b69f42b5844d59cf4175b389d59d774baf0b68e7
afreuljoy <afreuljoy@gmail.com>
2022-10-29 16:38:23 +0600

    java applying

---
+++ app/src/main/java/com/example/lab_03/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.lab_03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText et1 = (EditText) findViewById(R.id.et1);
+
+
+    }
+}
